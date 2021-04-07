70d3449216f88635d90c933b8e755619e9542928
Moon Hee Woong <silversaber89@gmail.com>
2021-04-07 13:01:41 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,3 +8,10 @@ curl -fsSL https://get.docker.com/ | sh
 # docker-compose install
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
+
+docker run -d \
+    --name=jdownloader-2 \
+    -p 5800:5800 \
+    -v /config:/config:rw \
+    -v /output:/output:rw \
+    jlesage/jdownloader-2
